eb316d745590036b3ece7e22dfd3bd01ac4f17a9
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-01-26 00:18:45 +0530

    Create Problem_2.R

---
+++ Assignment_1/Problem_2.R
@@ -0,0 +1,5 @@
+# Create and store a sequence of values from 5 to -11 that progresses in steps of 0.3
+a <- seq(5, -11, by = -0.3)
+# Overwrite the object from (1.) using the same sequence with the order reversed.
+a <- rev(a)
+print(a)

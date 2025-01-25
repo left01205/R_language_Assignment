208e73cc56e6463f0787895224deee6051c89e36
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-01-26 01:30:49 +0530

    Create Problem_7.R

---
+++ Assignment_1/Problem_7.R
@@ -0,0 +1,21 @@
+# Part i: Sequence of length 5 from 3 to 6
+seq1 <- seq(3, 6, length.out = 5)
+
+# Part ii: Twofold repetition of the vector c(2, -5.1, -33)
+seq2 <- rep(c(2, -5.1, -33), times = 2)
+
+# Part iii: The value 7/42 + 2
+seq3 <- 7 / 42 + 2
+
+# Combine all parts into a single vector
+final_vector <- c(seq1, seq2, seq3)
+
+# Display the result
+print(final_vector)
+
+# Output
+# [1]   3.000000   3.750000   4.500000   5.250000   6.000000   2.000000  -5.100000
+# [8] -33.000000   2.000000  -5.100000 -33.000000   2.166667
+# Extract the first and last elements of your vector from (6.), storing them as a new object.
+seq4 <- c(final_vector[1], final_vector[length(final_vector)])
+print(seq4)

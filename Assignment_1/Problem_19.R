9c1584ead6fd3c031376c2fa38860cef87ddfc68
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-01-30 18:52:42 +0530

    Create Problem_19.R

---
+++ Assignment_1/Problem_19.R
@@ -0,0 +1,9 @@
+# Vector Repetition and Order Sorting:
+# For the vector c(-2, 4, -6, 8, -10), describe how to repeat the vector itself three times 
+# and each of its elements 5 times. Following this, how would you sort the resulting vector 
+# from smallest to largest?
+
+
+Vec1 <- c(-2, 4, -6, 8, -10)
+S1 <- rep(x = Vec1 , times = 3 , each = 5)
+print(S1)

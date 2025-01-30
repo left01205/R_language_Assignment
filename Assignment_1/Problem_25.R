48a73a566decd7a0f84ad0593d8e01238842f2d6
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-01-30 19:04:20 +0530

    Create Problem_25.R

---
+++ Assignment_1/Problem_25.R
@@ -0,0 +1,8 @@
+# Conditional Vector Transformation:
+# Convert the vector c(3,1,2,3,1,2,3,1,2) into a vector of only 2s, using a vector of length 2. 
+# Which vectorized operations in R would facilitate this conversion?
+
+Vect2 <- c(c(3,1,2,3,1,2,3,1,2) != 2)
+Vect2[c(3,6,9)] <- TRUE
+Sol2 <- as.numeric(Vect2) * 2L
+print(Sol2)

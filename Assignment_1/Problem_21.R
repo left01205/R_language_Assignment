1836609733a219ce57678c57e04ef863d6a5b603
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-01-30 18:57:25 +0530

    Create Problem_21.R

---
+++ Assignment_1/Problem_21.R
@@ -0,0 +1,9 @@
+# Length Confirmation of a Complex Vector:
+# After creating the complex vector described above, what function would you use to confirm its
+# length is 25? What result from this function would indicate success?
+
+# Using length() function will provide us with the length of the vector.After computing the length 
+# of the vector, we use '==' operator to to check for length being equal to the required length 
+
+
+Sol1 <- vector(mode = "complex" , length = 25L)

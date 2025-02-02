410fdcd831cb1b23e432682ac314e857724cc66e
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-02-02 18:29:07 +0530

    Update Problem_20.R

--- Assignment_2/Problem_20.R
+++ Assignment_2/Problem_20.R
@@ -2,17 +2,20 @@
 # question (17.) with the average of the four corner elements of that matrix.
 
 
-avr <- mean(c(matrix_17[2,1],matrix_17[2,3],matrix_17[5,1],matrix_17[5,3]))
+avr <- mean(c(matrix_17[1,1],matrix_17[1,3],matrix_17[5,1],matrix_17[5,3]))
 matrix_17[2,1] <- avr
 matrix_17[2,3] <- avr
 matrix_17[5,1] <- avr
 matrix_17[5,3] <- avr
+print(avr)
 print(matrix_17)
 
 
-#     [,1] [,2] [,3]
-#[1,]  5.0   10 15.0
-#[2,]  7.5    9  7.5
-#[3,]  3.0    8 13.0
-#[4,]  2.0    7 12.0
-#[5,]  7.5    6  7.5
+# [1] 9.375
+
+#      [,1] [,2]   [,3]
+#[1,] 5.000   10 15.000
+#[2,] 9.375    9  9.375
+#[3,] 3.000    8 13.000
+#[4,] 2.000    7 12.000
+#[5,] 9.375    6  9.375

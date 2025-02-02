0165bcb8922519a6681de65ca88622e66c290a3d
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-02-02 16:58:09 +0530

    Create Problem_18.R

---
+++ Assignment_2/Problem_18.R
@@ -0,0 +1,12 @@
+# What would the result be if you remove the second row and the third column from the sorted
+# matrix in question (17.)? Ensure the result remains a matrix.
+
+result_18 <- matrix_17[-2, -3]
+print(result_18)
+
+
+#      [,1] [,2]
+#[1,]    5   10
+#[2,]    3    8
+#[3,]    2    7
+#[4,]    1    6

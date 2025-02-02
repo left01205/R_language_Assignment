522cc416a8134acf51a59cd1911805e37f8e9e3e
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-02-02 16:59:08 +0530

    Create Problem_19.R

---
+++ Assignment_2/Problem_19.R
@@ -0,0 +1,10 @@
+# From the matrix in question (17.), create a new 2 x 2 matrix using the top four 
+# elements of the last column.
+
+
+new_matrix_19 <- matrix(matrix_17[1:4, 3], nrow = 2, ncol = 2)
+print(new_matrix_19)
+
+#     [,1] [,2]
+#[1,]   15   13
+#[2,]   14   12

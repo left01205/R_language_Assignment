264b1c1a543677a3502c73235de66bbf5fd646f0
Arnav Jha <91931013+left01205@users.noreply.github.com>
2025-02-02 13:23:20 +0530

    Create Problem_2.R

---
+++ Assignment_2/Problem_2.R
@@ -0,0 +1,12 @@
+#  Construct and store a 4 x 2 matrix that’s filled row-wise with the values 4.3, 3.1
+#  ,8.2, 8.2, 3.2,0.9, 1.6, and 6.5, in that order.
+
+mat <- matrix(c(4.3, 3.1, 8.2, 8.2, 3.2, 0.9, 1.6, 6.5), nrow = 4, ncol=2, byrow = TRUE)
+print(mat)
+
+
+# Confirm the dimensions of the matrix from (1.) are 3 x 2 if you remove any one row.
+
+
+print(dim(mat[-1,]) == c(3,2))
+# If output is TRUE TRUE , then it is confirmed that the dimension of the matrix id 3 x 2
